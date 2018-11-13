31f994934ab78c0c690034c8f37954d6e6ed6cb7
Cristian David <cristian.diaz2@unmsm.edu.pe>
2018-11-13 17:08:48 -0500

    Incremento 3 TCS

--- admin/src/pages/Dashboard.vue
+++ admin/src/pages/Dashboard.vue
@@ -1,21 +1,73 @@
 <template>
-  <div id="pageDashboard">
-    <v-container grid-list-xl fluid>
-    </v-container>
-  </div>
+  <v-container>
+    
+    <highline v-if="listoV && listoC" :ventas="ventas" :compras="compras"></highline>
+    
+    <highpie v-if="listoP" :products="products"></highpie>
+    
+  </v-container>
 </template>
 
 <script>
-
+import highline from '@/components/HighLine'
+import highpie from '@/components/HighPie'
+import {HTTP} from '../http-commons.js'
 export default {
   components: {
-     
+     highline,
+     highpie
   },
-  data: () => ({
-  }),
+  data(){
+    return{
+      ventas:[],
+      compras:[],
+      products:[],
+      listoV:false,
+      listoC:false,
+      listoP:false
+    }
+  },
+  mounted(){
+    this.getProducts()
+   this.getCompras()
+   this.getVentas() 
+  },
   computed: {
    
   },
+  methods:{
+    getVentas:function(){
+      HTTP.url_detalle.get('total_venta').then(res=>{      
+            this.ventas = res.data
+            this.listoV = true
+          })
+          .catch(err=>{
+            console.log(err)
+          })
+    },
+    getCompras:function(){
+      HTTP.url_product.get('total_compra').then(res=>{
+        this.compras=res.data
+        this.listoC = true
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    },
+    getProducts:function(){
+      HTTP.url_product.get('totalProductos').then(res=>{
+        this.products = res.data
+        this.listoP = true
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    }
+  }
 
 };
 </script>
+
+<style>
+
+</style>
